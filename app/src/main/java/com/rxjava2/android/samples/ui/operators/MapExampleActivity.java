a37b0b400ff173f7ed450fb427e7bb6fdc3f6ec0
liupeng <717219917@qq.com>
2018-01-31 12:07:28 +0800

    sasmpile

--- app/src/main/java/com/rxjava2/android/samples/ui/operators/MapExampleActivity.java
+++ app/src/main/java/com/rxjava2/android/samples/ui/operators/MapExampleActivity.java
@@ -6,7 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.rxjava2.android.samples.R;
 import com.rxjava2.android.samples.model.ApiUser;
 import com.rxjava2.android.samples.model.User;
@@ -14,6 +16,7 @@ import com.rxjava2.android.samples.utils.AppConstant;
 import com.rxjava2.android.samples.utils.Utils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -23,6 +26,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import rx.functions.Action1;
 
 /**
  * Created by amitshekhar on 27/08/16.
@@ -40,81 +44,58 @@ public class MapExampleActivity extends AppCompatActivity {
         btn = (Button) findViewById(R.id.btn);
         textView = (TextView) findViewById(R.id.textView);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doSomeWork();
-            }
-        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                doSomeWork();
+//            }
+//        });
+
+
+        RxView.clicks(btn)
+                .throttleFirst(3, TimeUnit.SECONDS)
+                .subscribe(new Action1<Void>() {
+                    @Override  public void call(Void aVoid) {
+//                        Toast.makeText(MapExampleActivity.this, "ceshi", Toast.LENGTH_SHORT).show();
+                        doSomeWork();
+                    }
+                });
     }
 
     /*
-    * Here we are getting ApiUser Object from api server
-    * then we are converting it into User Object because
-    * may be our database support User Not ApiUser Object
-    * Here we are using Map Operator to do that
+   在这里，我们从API服务器获取ApiUser对象，然后我们将其转换为用户对象，
+   因为可能是我们的数据库支持用户不是ApiUser对象在这里，
+   我们正在使用地图运算符来做到这一点
     */
     private void doSomeWork() {
-        getObservable()
-                // Run on a background thread
-                .subscribeOn(Schedulers.io())
-                // Be notified on the main thread
-                .observeOn(AndroidSchedulers.mainThread())
+        Observable.create(new ObservableOnSubscribe<List<ApiUser>>() {    //原始数据格式
+            @Override  public void subscribe(ObservableEmitter<List<ApiUser>> e) throws Exception {  //是否弃置
+                    e.onNext(Utils.getApiUserList());                   //发送获取到的数据 未转化
+                    e.onComplete();                                               //注册oncomplete事件 否则接收不到
+                  } }  )
+                .subscribeOn(Schedulers.io())                             //后台线程运行
+                .observeOn(AndroidSchedulers.mainThread())//通知主线程
                 .map(new Function<List<ApiUser>, List<User>>() {
 
                     @Override
                     public List<User> apply(List<ApiUser> apiUsers) throws Exception {
-                        return Utils.convertApiUserListToUserList(apiUsers);
+                        return Utils.convertApiUserListToUserList(apiUsers);//从服务器转化为本地bean
                     }
                 })
-                .subscribe(getObserver());
-    }
-
-    private Observable<List<ApiUser>> getObservable() {
-        return Observable.create(new ObservableOnSubscribe<List<ApiUser>>() {
-            @Override
-            public void subscribe(ObservableEmitter<List<ApiUser>> e) throws Exception {
-                if (!e.isDisposed()) {
-                    e.onNext(Utils.getApiUserList());
-                    e.onComplete();
-                }
-            }
-        });
-    }
-
-    private Observer<List<User>> getObserver() {
-        return new Observer<List<User>>() {
-
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, " onSubscribe : " + d.isDisposed());
-            }
-
-            @Override
-            public void onNext(List<User> userList) {
-                textView.append(" onNext");
-                textView.append(AppConstant.LINE_SEPARATOR);
-                for (User user : userList) {
-                    textView.append(" firstname : " + user.firstname);
-                    textView.append(AppConstant.LINE_SEPARATOR);
-                }
-                Log.d(TAG, " onNext : " + userList.size());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                textView.append(" onError : " + e.getMessage());
-                textView.append(AppConstant.LINE_SEPARATOR);
-                Log.d(TAG, " onError : " + e.getMessage());
-            }
-
-            @Override
-            public void onComplete() {
-                textView.append(" onComplete");
-                textView.append(AppConstant.LINE_SEPARATOR);
-                Log.d(TAG, " onComplete");
-            }
-        };
+                .subscribe(new Observer<List<User>>() {
+                    @Override public void onSubscribe(Disposable d) {  Log.d(TAG, " onSubscribe : " + d.isDisposed());  } //是否弃置
+                    @Override public void onNext(List<User> userList) { //接收到转换后的数据 并处理
+                        textView.append(" onNext");
+                        textView.append(AppConstant.LINE_SEPARATOR);
+                        for (User user : userList) {
+                            textView.append(" firstname : " + user.firstname);
+                            textView.append(AppConstant.LINE_SEPARATOR);
+                        }
+                        Log.d(TAG, " onNext : " + userList.size());
+                    }
+                    @Override public void onError(Throwable e) {  Log.d(TAG, " onError : " + e.getMessage()); } //所有异常
+                    @Override public void onComplete() {  Log.d(TAG, " onComplete"); }//事件完成之后
+                });
     }
 
 
